a32182617c65f8391542fef9fc6d1095e9f7054c
bigendiansmalls <mainframe@bigendiansmalls.com>
2019-02-10 14:21:57 -0600

    Shells

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-/usr/bin/java --module-path /Users/chad/Downloads/javafx-sdk-11.0.2/lib --add-modules=javafx.controls -Dfile.encoding=UTF-8 -jar /Users/chad/WorkInProgress/Xmit/XmitMe.jar
-
+nohup /usr/bin/java \
+--module-path /Users/chad/Downloads/javafx-sdk-11.0.2/lib \
+--add-modules=javafx.controls -Dfile.encoding=UTF-8 \
+-jar /Users/chad/WorkInProgress/Xmit/XmitMe.jar >nohup.out 2>&1 &
