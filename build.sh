83ce1b8eed230010330d801f2e20e03c9e0ee84e
bigendiansmalls <mainframe@bigendiansmalls.com>
2019-02-10 14:22:07 -0600

    Shells

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 javac -cp "/Users/chad/Downloads/javafx-sdk-11.0.2/lib/*" $(find . -name "*.java")
 
-cat<<EOF> m.txt
+cat<<EOF> manifest
 Manifest-Version: 1.0
 Ant-Version: Apache Ant 1.10.5
 Created-By: 11.0.2+7 (Oracle Corporation)
